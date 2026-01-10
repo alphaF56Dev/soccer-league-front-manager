ea8ff2fb46c58ff6f3a47c13b08c1a411815dd95
Alejandro Lopez <aldoss5623@gmail.com>
2026-01-09 18:49:35 -0600

    adjusting interceptor to valdate all of the time the user is authenticated in a global way

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,14 +1,24 @@
 import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, throwError } from "rxjs";
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+    const router = inject(Router);
     const token = sessionStorage.getItem("jwt");
-    if(token && !req.url.includes('/users/auth')){
-        const cloned = req.clone({
-            setHeaders: {
-                Authorization: `Bearer ${token}`
+
+    const isAuthEndPoint = req.url.includes('/users/auth');
+    
+    const authReq = token ? req.clone({setHeaders: {Authorization : `Bearer ${token}`}}) : req;
+
+    return next(authReq).pipe(
+        catchError(error => {
+            //route to login if the backend give us 403 or 401
+            if(error.status === 401 || error.status === 403 ){
+                sessionStorage.removeItem('jwt');
+                router.navigate(['/login']);
             }
-        });
-        return next(cloned);
-    }
-    return next(req);
+            return throwError(()=>error);
+        })
+    )
 }
